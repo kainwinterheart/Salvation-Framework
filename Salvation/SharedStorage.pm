d39b11cb8f600cfb9be509940e2edf0b3a2add12
Genadiy Filatov <g.filatov@jimmy.drweb.com>
2013-04-29 17:08:51 +0400

    now it can be hooked via following new methods: add_before_get_handler, add_around_get_handler, add_after_get_handler, add_before_put_handler, add_around_put_handler, add_after_put_handler

--- Salvation/SharedStorage.pm
+++ Salvation/SharedStorage.pm
@@ -4,7 +4,102 @@ package Salvation::SharedStorage;
 
 use Moose;
 
+
 has 'data'	=> ( is => 'rw', isa => 'HashRef', lazy => 1, default => sub{ {} } );
+
+
+foreach my $method ( ( 'get', 'put' ) )
+{
+	foreach my $event ( ( 'before', 'around', 'after' ) )
+	{
+		my $storage = sprintf( '__%s_%s_handlers', $event, $method );
+
+		has $storage => ( is => 'rw', isa => 'ArrayRef[CodeRef]', lazy => 1, default => sub{ [] }, init_arg => undef );
+
+		{
+			my $accessor = sprintf( 'add_%s_%s_handler', $event, $method );
+
+			no strict 'refs';
+
+			*$accessor = sub
+			{
+				my ( $self, $code ) = @_;
+
+				if( $event eq 'around' )
+				{
+					if( my $orig = shift @{ $self -> $storage() } )
+					{
+						unshift @{ $self -> $storage() }, sub
+						{
+							my ( $lorig, $self, @rest ) = @_;
+
+							return $code -> ( sub{ $orig -> ( $lorig, $self, @rest ) }, $self, @rest );
+						};
+
+					} else
+					{
+						push @{ $self -> $storage() }, $code;
+					}
+
+				} elsif( $event eq 'after' )
+				{
+					push @{ $self -> $storage() }, $code;
+
+				} elsif( $event eq 'before' )
+				{
+					unshift @{ $self -> $storage() }, $code;
+
+				} else
+				{
+					die sprintf( 'Unknown event: %s', $event );
+				}
+
+				return 1;
+			};
+
+			use strict 'refs';
+		}
+	}
+
+	before $method => sub
+	{
+		my $self = shift;
+
+		my $storage = sprintf( '__before_%s_handlers', $method );
+
+		foreach my $code ( @{ $self -> $storage() } )
+		{
+			$code -> ( $self );
+		}
+	};
+
+	around $method => sub
+	{
+		my ( $orig, $self, @rest ) = @_;
+
+		my $storage = sprintf( '__around_%s_handlers', $method );
+
+		if( my $method = $self -> $storage() -> [ 0 ] )
+		{
+			return $method -> ( $orig, $self, @rest );
+		}
+
+		return $self -> $orig( @rest );
+	};
+
+	after $method => sub
+	{
+		my $self = shift;
+
+		my $storage = sprintf( '__after_%s_handlers', $method );
+
+		foreach my $code ( @{ $self -> $storage() } )
+		{
+			$code -> ( $self );
+		}
+	};
+}
+
 
 sub put
 {
