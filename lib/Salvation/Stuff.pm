96c790b1f88f69db37382b635a0c0c3a1b78c2c4
Kain Winterheart <kain@azazel.local>
2013-05-21 16:07:58 +0400

    fix for &Salvation::Stuff::load_class + new function &Salvation::Stuff::is_namespace_present( $ ) + some simple tests

--- lib/Salvation/Stuff.pm
+++ lib/Salvation/Stuff.pm
@@ -8,7 +8,8 @@ our @ISA 	 = ( 'Exporter' );
 our @EXPORT 	 = ();
 
 our @EXPORT_OK 	 = ( '&full_pkg',
-		     '&load_class' );
+		     '&load_class',
+		     '&is_namespace_present' );
 
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );
 our $VERSION 	 = 1.00;
@@ -22,15 +23,41 @@ sub load_class
 {
 	my $class = shift;
 
-	my $path = join( '/', split( /\:\:/, $class ) ) . '.pm';
+	return 1 if &is_namespace_present( $class );
 
-	return ( exists( $INC{ $path } ) or eval
-	{
-		require $path;
-		1;
-	} );
+	require Module::Load;
+
+	eval{ &Module::Load::load( $class ) };
+
+	return 1 if &is_namespace_present( $class );
+
+	require Module::Loaded;
+
+	return ( &Module::Loaded::is_loaded( $class ) ? 1 : 0 );
 }
 
+sub is_namespace_present
+{
+	my $ns = shift;
+
+	my @parts = split( /\:\:/, $ns );
+	my $ok    = 0;
+	my $node  = undef;
+
+	foreach my $part ( @parts )
+	{	
+		if( $node = ( $node //= *::{ 'HASH' } ) -> { sprintf( '%s::', $part ) } )
+		{
+			++$ok;
+
+		} else
+		{
+			last;
+		}
+	}
+
+	return ( scalar( @parts ) == $ok );
+}
 
 -1;
 
