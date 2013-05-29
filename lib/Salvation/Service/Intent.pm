5fe3ba85996bc9f6da98a7ee4737dbc5c7173b7a
Gennadiy Filatov <g.filatov@drweb.com>
2013-05-29 14:58:55 +0400

    You can now call service's methods on intent (which are not overriden by intent itself)

--- lib/Salvation/Service/Intent.pm
+++ lib/Salvation/Service/Intent.pm
@@ -8,6 +8,10 @@ with 'Salvation::Roles::AppArgs', 'Salvation::Roles::DataSet', 'Salvation::Roles
 
 use Salvation::Stuff '&load_class';
 
+use Carp 'confess';
+
+use Scalar::Util 'blessed';
+
 has '__service'	=> ( is => 'ro', isa => 'Str', required => 1, lazy => 1, default => undef, init_arg => 'service' );
 
 has 'service' => ( is => 'ro', isa => 'Salvation::Service', builder => '_build_service', lazy => 1, init_arg => undef );
@@ -21,13 +25,35 @@ sub _build_service
 	) : undef );
 }
 
-sub main
 {
+	our $AUTOLOAD;
+
+	sub AUTOLOAD
+	{
+		my ( $self, @rest ) = @_;
+
+		( my $method = $AUTOLOAD ) =~ s/^.*\:\://;
+
+		my $o = $self -> service();
+
+		return $o -> $method( @rest ) if $o -> can( $method );
+
+		confess( sprintf( 'Nor %s, nor %s has method %s', ( ref( $self ) or $self ), ( ref( $o ) or $o ), $method ) );
+	}
 }
 
-sub start
+sub can
 {
-	return shift -> service() -> main();
+	my ( $self, @rest ) = @_;
+
+	if( defined( my $code = $self -> SUPER::can( @rest ) ) )
+	{
+		return $code;
+	}
+
+	return $self -> service() -> can( @rest ) if blessed $self;
+
+	return undef;
 }
 
 no Moose;
