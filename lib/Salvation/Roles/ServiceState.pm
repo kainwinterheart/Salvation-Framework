e706fd123977986858ee882628707a2e5df2692e
Gennadiy Filatov <g.filatov@drweb.com>
2013-12-30 13:42:32 +0400

    docs

--- lib/Salvation/Roles/ServiceState.pm
+++ lib/Salvation/Roles/ServiceState.pm
@@ -10,3 +10,25 @@ no Moose::Role;
 
 -1;
 
+# ABSTRACT: Service reference definition
+
+=pod
+
+=head1 NAME
+
+Salvation::Roles::ServiceState - Service state reference definition
+
+=head1 REQUIRES
+
+L<Moose::Role> 
+
+=head1 METHODS
+
+=head2 state
+
+ $self -> state();
+
+Return appropriate L<Salvation::Service::State> object instance.
+
+=cut
+
