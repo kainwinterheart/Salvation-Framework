3cbca6927efef9a0161038ecbe7e6983857aea14
Gennadiy Filatov <g.filatov@drweb.com>
2013-12-30 13:46:19 +0400

    docs

--- lib/Salvation/Roles/SystemReference.pm
+++ lib/Salvation/Roles/SystemReference.pm
@@ -10,3 +10,25 @@ no Moose::Role;
 
 -1;
 
+# ABSTRACT: System reference definition
+
+=pod
+
+=head1 NAME
+
+Salvation::Roles::SystemReference - System reference definition
+
+=head1 REQUIRES
+
+L<Moose::Role> 
+
+=head1 METHODS
+
+=head2 system
+
+ $self -> system();
+
+Return appropriate L<Salvation::System>-derived object instance.
+
+=cut
+
