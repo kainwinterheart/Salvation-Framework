bfe8a96073d74e53635190c6e3243d9807ced207
Gennadiy Filatov <g.filatov@drweb.com>
2013-12-30 11:40:07 +0400

    docs

--- lib/Salvation/Service/DataSet.pm
+++ lib/Salvation/Service/DataSet.pm
@@ -61,3 +61,78 @@ no Moose;
 
 -1;
 
+# ABSTRACT: Base class for DataSet
+
+=pod
+
+=head1 NAME
+
+Salvation::Service::DataSet - Base class for DataSet
+
+=head1 SYNOPSIS
+
+ package YourSystem::Services::SomeService::DataSet;
+
+ use Moose;
+
+ extends 'Salvation::Service::DataSet';
+
+ no Moose;
+
+=head1 REQUIRES
+
+L<Moose> 
+
+=head1 DESCRIPTION
+
+=head2 Applied roles
+
+L<Salvation::Roles::ServiceReference>
+
+=head1 METHODS
+
+=head2 To be called
+
+=head3 fetch
+
+ $dataset -> fetch();
+
+Returns next row from the DataSet moving current position of internal iterator one step forward.
+
+=head3 first
+
+ $dataset -> first();
+
+Returns first row from the DataSet.
+
+=head3 get
+
+ $dataset -> get();
+ $dataset -> get( $index );
+
+Returns a row from the DataSet. C<$index> is an integer starting from zero. If C<$index> is omitted - current position of internal iterator is used.
+
+=head3 last
+
+ $dataset -> last();
+
+Returns last row from the DataSet.
+
+=head3 seek
+
+ $dataset -> seek( $index );
+
+Sets position of internal iterator. C<$index> is an integer starting from zero.
+
+=head2 To be redefined
+
+You can redefine following methods to achieve your own goals.
+
+=head3 main
+
+Should return an ArrayRef.
+One element - one row. Each element is an abstract object that will be processed by a view, or somehow else if you want to.
+The only argument is C<$self> which is current view's instance.
+
+=cut
+
