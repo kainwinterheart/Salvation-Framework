f297d3b1534f134f2f50e3a50fc1b6b2ea68ca1a
Gennadiy Filatov <g.filatov@drweb.com>
2013-12-30 13:35:50 +0400

    docs

--- lib/Salvation/Roles/AppArgs.pm
+++ lib/Salvation/Roles/AppArgs.pm
@@ -10,3 +10,26 @@ no Moose::Role;
 
 -1;
 
+# ABSTRACT: Application arguments definition
+
+=pod
+
+=head1 NAME
+
+Salvation::Roles::AppArgs - Application arguments definition
+
+=head1 REQUIRES
+
+L<Moose::Role> 
+
+=head1 ATTRIBUTES
+
+=head2 args
+
+ $self -> args()
+ $self -> args( \%args )
+
+An arguments HashRef.
+
+=cut
+
